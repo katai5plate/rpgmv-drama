9c67bde7b0a4464c5814dbd09570cf5a6b8d4f45
はどはど / Hadhad / Had2Apps <katai5plate.sub@gmail.com>
2018-10-17 19:05:31 +0900

    Create example.js

---
+++ src/example.js
@@ -0,0 +1,30 @@
+drama.chara({
+  "A-san": ["Actor", 1],
+  "B-san": ["Actor", 2],
+  "C-san": ["Actor", 3],
+})
+
+drama.write("opening", [
+  [
+    `> A-san
+    odkoqkdokodqwkokdw
+    odkoqkdokodqwkokdw
+    odkoqkdokodqwkokdw
+    odkoqkdokodqwkokdw`,
+    `> B-san
+    odkoqkdokodqwkokdw
+    odkoqkdokodqwkokdw`,
+    `> C-san
+    odkoqkdokodqwkokdw
+    odkoqkdokodqwkokdw
+    
+    odkoqkdokodqwkokdw`,
+    `> 
+    odkoq\c[6]kdokodqwkokdw
+    odkoqkdokodqwkokdw
+    odkoqkdokodqwkokdw
+    odkoqkdok\c[0]odqwkokdw`
+  ]
+])
+
+// drama.read("opening", 0);
