bb7102f79153888dbb5a90a53e25adea47a3fe08
はどはど / Hadhad / Had2Apps <katai5plate.sub@gmail.com>
2018-10-18 17:07:01 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,71 @@
-const gulp = require('gulp'),
-  plumber = require('gulp-plumber'),
-  babel = require('gulp-babel'),
-  uglify = require('gulp-uglify'),
-  rename = require('gulp-rename'),
-  jsfuck = require('gulp-jsfuck'),
-  beautify = require("gulp-beautify");
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const babel = require("gulp-babel"),
+  babel_opt = { presets: ["@babel/env"] };
+const uglify = require("gulp-uglify"),
+  uglify_opt = { output: { comments: /^:/ } },
+  uglify_opt_books = {};
+const rename = require("gulp-rename"),
+  rename_opt_min = { extname: ".min.js" },
+  rename_opt_safe = { extname: ".safe.js" };
+const beautify = require("gulp-beautify"),
+  beautify_opt = { indent_size: 2 };
+const concat = require("gulp-concat");
 
-gulp.task('build', function (cb) {
-  return gulp.src('src/**/*.js')
+const path = {
+  src: {
+    all: "src/**/*.js",
+    plugin: "src/*.js",
+    books: "src/books/**/*.js"
+  },
+  out: {
+    all: "out/",
+    books: "out/books/"
+  },
+  name: {
+    shelf: "book-shelf.js"
+  }
+};
+
+gulp.task("build", function () {
+  return gulp
+    .src(path.src.plugin)
     .pipe(plumber())
-    .pipe(rename({ extname: '.safe.js' }))
-    .pipe(babel({ presets: ['@babel/env'] }))
-    .pipe(beautify({ indent_size: 2 }))
-    .pipe(gulp.dest('out/'))
-    .pipe(rename({
-      extname: '.min.js'
-    }))
-    .pipe(uglify())
-    .pipe(gulp.dest('out/'))
+    .pipe(rename(rename_opt_safe))
+    .pipe(babel(babel_opt))
+    .pipe(beautify(beautify_opt))
+    .pipe(gulp.dest(path.out.all))
+    .pipe(rename(rename_opt_min))
+    .pipe(uglify(uglify_opt))
+    .pipe(gulp.dest(path.out.all));
 });
 
-gulp.task('fuck-es6', function () {
-  return gulp.src('src/**/*.js')
+gulp.task("build-all", function () {
+  return gulp
+    .src(path.src.all)
     .pipe(plumber())
-    .pipe(rename({ extname: '.fuck.js' }))
-    .pipe(jsfuck())
-    .pipe(gulp.dest('out/'))
-});
-gulp.task('fuck-you', function () {
-  return gulp.src('src/**/*.js')
-    .pipe(plumber())
-    .pipe(rename({ extname: '.fuck.you.js' }))
-    .pipe(babel({ presets: ['@babel/env'] }))
-    .pipe(jsfuck())
-    .pipe(gulp.dest('out/'))
+    .pipe(rename(rename_opt_safe))
+    .pipe(babel(babel_opt))
+    .pipe(beautify(beautify_opt))
+    .pipe(gulp.dest(path.out.all))
+    .pipe(rename(rename_opt_min))
+    .pipe(uglify(uglify_opt))
+    .pipe(gulp.dest(path.out.all));
 });
 
-gulp.task('default', ['build']);
+gulp.task("archive", function () {
+  return gulp
+    .src(path.src.books)
+    .pipe(plumber())
+    .pipe(concat(path.name.shelf))
+    // .pipe(gulp.dest(path.out.books))
+    // .pipe(rename(rename_opt_safe))
+    .pipe(babel(babel_opt))
+    // .pipe(beautify(beautify_opt))
+    // .pipe(gulp.dest(path.out.books))
+    // .pipe(rename(rename_opt_min))
+    .pipe(uglify(uglify_opt_books))
+    .pipe(gulp.dest(path.out.books));
+});
 
-gulp.task('fuck', ['fuck-es6', 'fuck-you']);
+gulp.task("default", ["build-all", "archive"]);
