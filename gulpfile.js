4fe2d59d666bc445b179e1259c4fa4f038f108de
はどはど / Hadhad / Had2Apps <katai5plate.sub@gmail.com>
2018-10-18 15:32:47 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,41 @@
-const gulp = require("gulp"),
-  plumber = require("gulp-plumber"),
-  babel = require("gulp-babel"),
-  uglify = require("gulp-uglify"),
-  rename = require("gulp-rename");
+const gulp = require('gulp'),
+  plumber = require('gulp-plumber'),
+  babel = require('gulp-babel'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+  jsfuck = require('gulp-jsfuck'),
+  beautify = require("gulp-beautify");
 
-gulp.task("build", function() {
-  return gulp
-    .src("src/H2A_drama.js")
+gulp.task('build', function (cb) {
+  return gulp.src('src/**/*.js')
     .pipe(plumber())
-    .pipe(
-      rename({
-        extname: ".safe.js"
-      })
-    )
-    .pipe(
-      babel({
-        presets: ["@babel/env"]
-      })
-    )
-    .pipe(gulp.dest("out/"))
-    .pipe(
-      rename({
-        extname: ".safe.min.js"
-      })
-    )
+    .pipe(rename({ extname: '.safe.js' }))
+    .pipe(babel({ presets: ['@babel/env'] }))
+    .pipe(beautify({ indent_size: 2 }))
+    .pipe(gulp.dest('out/'))
+    .pipe(rename({
+      extname: '.min.js'
+    }))
     .pipe(uglify())
-    .pipe(gulp.dest("out/"));
+    .pipe(gulp.dest('out/'))
 });
 
-gulp.task("default", ["build"]);
+gulp.task('fuck-es6', function () {
+  return gulp.src('src/**/*.js')
+    .pipe(plumber())
+    .pipe(rename({ extname: '.fuck.js' }))
+    .pipe(jsfuck())
+    .pipe(gulp.dest('out/'))
+});
+gulp.task('fuck-you', function () {
+  return gulp.src('src/**/*.js')
+    .pipe(plumber())
+    .pipe(rename({ extname: '.fuck.you.js' }))
+    .pipe(babel({ presets: ['@babel/env'] }))
+    .pipe(jsfuck())
+    .pipe(gulp.dest('out/'))
+});
+
+gulp.task('default', ['build']);
+
+gulp.task('fuck', ['fuck-es6', 'fuck-you']);
